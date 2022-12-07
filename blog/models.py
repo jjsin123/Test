3b97d57283177b28c5866a5098312b3b94464999
jjsin123 <jjsin1234@gmail.com>
2022-12-07 03:41:23 +0000

    블로그 리스트 이미지 업데이트

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,10 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
     
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d',blank=True)
+    # upload_to 는 이미지를 저장 할 경로
+    # black=True는 이미지를 저장하지 않고 비워놔도 된다는 뜻
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d',blank=True)
     create_at = models.DateTimeField(auto_now_add=True) # 시간이 자동으로 입력
     updated_at = models.DateTimeField(auto_now=True)
     
