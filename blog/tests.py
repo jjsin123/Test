a3a50f629c510bfcfc54442abd08d90343b56a08
jjsin123 <jjsin1234@gmail.com>
2022-12-08 06:10:48 +0000

    네비게이션바 푸터 업데이트

--- blog/tests.py
+++ blog/tests.py
@@ -7,6 +7,25 @@ class TestView(TestCase): # TestCase에서 상속 받아옴
     def setup(self):
         self.client -= Client()
         
+    def navbar_test(self,soup):
+        navbar = soup.nav
+        self.assertIn('Blog',navbar.text)
+        self.assertIn('About Me',navbar.text)
+        
+        logo_btn = navbar.find('a', text="Do It Django")
+        self.assertEqual(logo_btn.attrs['href'],'/')
+        
+        home_btn = navbar.find('a', text="Home")
+        self.assertEqual(logo_btn.attrs['href'],'/')
+        
+        blog_btn = navbar.find('a', text="Blog")
+        self.assertEqual(logo_btn.attrs['href'],'/blog/')
+        
+        about_me_btn = navbar.find('a', text="About Me")
+        self.assertEqual(logo_btn.attrs['href'],'/about_me/')
+        
+        
+        
     def test_post_list(self):
         # 1.1 포스트 목록페이지를 가져오는가
         response = self.client.get('/blog/')
@@ -17,13 +36,7 @@ class TestView(TestCase): # TestCase에서 상속 받아옴
         # 1.3 페이지 타이틀은 'Blog'인가
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text,'Blog')
-            
-        # 1.4 네비게이션 바가 있는가
-        navbar = soup.nav
-            
-        # 1.5 Blog, About Me라는 문구가 네비게이션 바에 있는가
-        self.assertIn('Blog',navbar.text)
-        self.assertIn('About Me',navbar.text)
+        self.navbar_test(soup)
         
         # 2.1 Post가 존재하지 않는가
         self.assertEqual(Post.objects.count(), 0)
@@ -75,10 +88,8 @@ class TestView(TestCase): # TestCase에서 상속 받아옴
         self.assertEqual(response.status_code, 200)
     
         # 2.2 포스트 목록페이지와 같은 navbar가 존재하는가
-        navbar = soup.nav
-        self.assertIn('Blog',navbar.text)
-        self.assertIn('About Me',navbar.text)
-    
+        self.navbar_test(soup)
+        
         # 2.3 첫번째 포스트의 제목이 웹브라우저 탭 타이틀에 존재하는가
         self.assertIn(post_001.title, soup.title.text)
         
