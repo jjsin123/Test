b338c8cd963438acc22c5193625da6d2d0ff8537
jjsin123 <jjsin1234@gmail.com>
2022-12-12 08:17:12 +0000

    tag update

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('tag/<str:slug>/', views.tag_page),
+    path('category/<str:slug>/', views.category_page),
     path('<int:pk>/',views.PostDetail.as_view()),
     # path('<int:pk',views.single_post_page),
     # path('',views.index),
     path('',views.PostList.as_view()),
-    
-    path('category/<str:slug>/', views.category_page)
+
 ]
