42c4c69c06c8b94e5107cb4857e19239d4770ae5
jjsin123 <jjsin1234@gmail.com>
2022-12-13 08:15:34 +0000

    Permission

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,17 @@
-
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post, Category , Tag
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import  render, redirect
+
+
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title','hook_text','content','head_image','file_upload','category','tags']
+    
+    
+    
+    
 
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
